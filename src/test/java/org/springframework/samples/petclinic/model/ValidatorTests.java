225cc5353fb1ceccb48ef2c8367f804e0e797344
Khalil HIMET <khalil.himet@outlook.com>
2020-08-24 15:57:16 +0200

    Tests Jenkins - correction de l'erreur dans le test "ValidatorTests.java"

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -30,7 +30,7 @@ class ValidatorTests {
 
         LocaleContextHolder.setLocale(Locale.ENGLISH);
         Person person = new Person();
-        person.setFirstName("hello");
+        person.setFirstName("");
         person.setLastName("smith");
 
         Validator validator = createValidator();
